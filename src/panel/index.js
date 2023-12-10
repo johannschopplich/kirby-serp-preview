04d375bfe659c1eb697b55e1802f359f81094bff
Johann Schopplich <mail@johannschopplich.com>
2023-12-10 16:32:07 +0100

    fix: internal Panel plugin name

--- src/panel/index.js
+++ src/panel/index.js
@@ -1,7 +1,7 @@
 import SerpPreview from "./components/SerpPreview.vue";
 import "./index.css";
 
-window.panel.plugin("johannschopplich/website", {
+window.panel.plugin("johannschopplich/serp-preview", {
   sections: {
     "serp-preview": SerpPreview,
   },
